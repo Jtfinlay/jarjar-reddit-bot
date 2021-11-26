b2594dd46bcaefc2282266722d4720cca72211a2
James Finlay <jtfinlay1@gmail.com>
2021-11-26 12:12:42 -0800

    has bot replied logic added

--- message_processor.py
+++ message_processor.py
@@ -1,15 +1,53 @@
 from colored import stylize, fg
 from dotenv import load_dotenv
+import json
 import os
 import re
+from types import SimpleNamespace
+
+
+IGNORE_PATTERN = "^!ignore$"
+KNOWN_BOTS = ["Obiwan-Kenobi-Bot", "sheev-bot", "Anakin_Skywalker_Bot"]
 
 load_dotenv()
 
-IGNORE_PATTERN = "^!ignore$"
+# load responses file into a simple object
+with open("responses.json") as fh:
+    string = fh.read()
+responses = json.loads(string, object_hook=lambda d: SimpleNamespace(**d))
 
-def checkForCommand(comment):
+#
+# Check if the comment is the '!ignore' command.
+#
+def checkForIgnoreCommand(comment):
     if re.search(IGNORE_PATTERN, comment.body, re.IGNORECASE):
-        author = comment.parent().author
-        print(stylize(f"\n'{author}' has been ignored by '{comment.author}'.", fg('cyan')))
+        author = comment.parent().author.name
+        print(stylize(f"\n'{author}' has been ignored by '{comment.author.name}'.", fg('cyan')))
         return author == os.getenv('REDDIT_USER')
     return False
+
+#
+# Check whether our bot has already replied to the comment.
+#
+def hasBotReplied(comment):
+    # First, check redis on whether we've replied or not.
+    # TODO
+
+    # Otherwise, look through the replies to find our bot's name.
+    for comment in comment.replies.list():
+        if comment.author.name == os.getenv('REDDIT_USER'):
+            return True
+
+    return False
+
+
+#
+# Look for a reply to this comment
+#
+def extractReply(comment):
+    if comment.author.name == os.getenv('REDDIT_USER'):
+        return
+    
+    if hasBotReplied(comment):
+        return
+    
--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from types import SimpleNamespace
 import unittest
 
-from message_processor import checkForCommand
+from message_processor import checkForIgnoreCommand, hasBotReplied
 
 load_dotenv()
 
@@ -10,39 +10,73 @@ class CommentStub(SimpleNamespace):
     def parent(self):
         return CommentStub(**self._parent)
 
+class RepliesStub(SimpleNamespace):
+    def list(self):
+        return self._replies
+
 class TestMessageProcessor(unittest.TestCase):
     def test_correctly_find_ignore_command(self):
         comment = CommentStub(**{
-            'author': 'user123',
+            'author': SimpleNamespace(**{ 'name': 'user123' }),
             'body': '!ignore',
             '_parent': {
-                'author': 'jarjar_bot'
+                'author': SimpleNamespace(**{ 'name': 'jarjar_bot' })
             }
         })
 
-        self.assertTrue(checkForCommand(comment))
+        self.assertTrue(checkForIgnoreCommand(comment))
 
     def test_dismiss_ignore_command_for_other_bot(self):
         comment = CommentStub(**{
-            'author': 'user123',
+            'author': SimpleNamespace(**{ 'name': 'user123' }),
             'body': '!ignore',
             '_parent': {
-                'author': 'a_different_bot'
+                'author': SimpleNamespace(**{ 'name': 'a_different_bot' })
             }
         })
 
-        self.assertFalse(checkForCommand(comment))
+        self.assertFalse(checkForIgnoreCommand(comment))
 
     def test_dismiss_non_ignore_command(self):
         comment = CommentStub(**{
-            'author': 'user123',
+            'author': SimpleNamespace(**{ 'name': 'user123' }),
             'body': 'Good bot',
             '_parent': {
-                'author': 'jarjar_bot'
+                'author': SimpleNamespace(**{ 'name': 'jarjar_bot' })
             }
         })
 
-        self.assertFalse(checkForCommand(comment))
+        self.assertFalse(checkForIgnoreCommand(comment))
+
+    def test_correctly_find_when_already_replied(self):
+        comment = CommentStub(**{
+            'author': SimpleNamespace(**{ 'name': 'user123' }),
+            'body': '!ignore',
+            'replies':
+                RepliesStub(**{
+                    '_replies': [CommentStub(**{
+                        'author': SimpleNamespace(**{'name': 'jarjar_bot'}),
+                        'body': 'Meesa your humble servant!'
+                    })]
+                })
+        })
+
+        self.assertTrue(hasBotReplied(comment))
+
+    def test_correctly_find_when_not_replied(self):
+        comment = CommentStub(**{
+            'author': SimpleNamespace(**{ 'name': 'user123' }),
+            'body': '!ignore',
+            'replies':
+                RepliesStub(**{
+                    '_replies': [CommentStub(**{
+                        'author': SimpleNamespace(**{'name': 'other_bot'}),
+                        'body': 'Meesa your humble servant!'
+                    })]
+                })
+        })
+
+        self.assertFalse(hasBotReplied(comment))
 
 
 if __name__ == '__main__':
