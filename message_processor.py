4827f9c112931bafbde766f23accfdf5fdef52d6
James Finlay <jtfinlay1@gmail.com>
2022-03-31 14:38:46 -0700

    some updates

--- message_processor.py
+++ message_processor.py
@@ -6,9 +6,9 @@ import random
 import re
 from types import SimpleNamespace
 
-BOT_RESPONSE_CHANCE = 0.1
+BOT_RESPONSE_CHANCE = 0.05
 IGNORE_PATTERN = "^!ignore$"
-KNOWN_BOTS = ["Obiwan-Kenobi-Bot", "sheev-bot", "Anakin_Skywalker_Bot"]
+KNOWN_BOTS = ["Obiwan-Kenobi-Bot", "sheev-bot", "Anakin_Skywalker_Bot", "Qui-Gon_Jinn_Bot"]
 
 load_dotenv()
 
